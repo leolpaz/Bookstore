dc661d6112c7b96c6ef632bf9ed6a0f2237a87fb
leolpaz <leonardolpaz95@gmail.com>
2021-11-01 16:09:38 -0300

    Fix formatting mistakes pointed by linters

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,20 +1,17 @@
-import React from "react";
+import React from 'react';
 
-const Books = () => {
-  return (
-    <>
-    <div className="books-container">
-    </div>
+const Books = () => (
+  <>
+    <div className="books-container" />
     <h2>ADD NEW BOOK</h2>
     <form>
-    <input type="text" placeholder="Book title"/>
-    <select disabled>
-    <option value="category">Category</option>
-    </select>
-    <button>ADD BOOK</button>
+      <input type="text" placeholder="Book title" />
+      <select disabled>
+        <option value="category">Category</option>
+      </select>
+      <button type="button">ADD BOOK</button>
     </form>
-    </>
-  )
-}
+  </>
+);
 
-export default Books
+export default Books;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
