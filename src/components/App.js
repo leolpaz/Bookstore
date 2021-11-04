a9cc1f1ecaeb1123ba109bc6c9ed65d9b32b2ad9
leolpaz <leonardolpaz95@gmail.com>
2021-11-04 13:36:14 -0300

    Added fonts

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configStore';
+import '../assets/App.css';
 
 // components
 
